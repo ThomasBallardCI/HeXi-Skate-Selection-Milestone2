c703b329a808afc7e40ac50d105274ee6091b661
ThomasBallardCI <ThomasGEBallard@gmail.com>
2023-02-22 16:25:55 +0000

    file name and code tidy

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,10 +6,12 @@ const question = document.getElementById('question');
 const image = document.getElementsByClassName('picture-area');
 const start = document.getElementsByClassName('start-btn');
 
+// Results panel Hidden
 document.getElementById("panel-2").style.display = "none";
 
+//---------------------------------------------------------
 // button selection color change
-var button = document.getElementsByClassName("btn");
+var button = document.getElementsByClassName("answer-btn");
 var addSelectedClass = function(){
     removeSelectClass();
     this.classList.add('selected');
@@ -24,13 +26,7 @@ for (var i =0; i < button.length; i++) {
     button[i].addEventListener("click", addSelectedClass);
 };
 
-
-// empty array to store selection responses
-let responses = [];
-
-// array for keep track of current question to display
-let currentQuestion = 0;
-
+// ---------------------------------------------------------
 // array of questions
 let questions = [
     {
@@ -39,25 +35,44 @@ let questions = [
     },
     {
     question: "What is your prefered price point?",
-    answers: ["100-250", ""]     
+    answers: ["£100-£250", "£250-£400", "£400+"]     
+    },
+    {
+    question: "What style of skating do you want to do?",
+    answers: ["Stunts", "Fitness", "Urban"]
     }
 ];
+
+//-----------------------------------------------------------
+let questionImages = [
+    {
+    image: 'url("/assets/images/PriceRange.png")'
+    }
+]
 
 // function to start quizz (to be called)
 function beginQuiz(){
     loadQuestion(0);
-
 }
 
+// function to load question data
 function loadQuestion(currentQuestion) {
     question.innerHTML = questions[currentQuestion].question;
     answerBtnA.innerHTML = questions[currentQuestion].answers[0];
     answerBtnB.innerHTML = questions[currentQuestion].answers[1];
     answerBtnC.innerHTML = questions[currentQuestion].answers[2];
+    image.style.backgroundImage = questionImages[currentImage].image;
 }
 
-// start Quiz from click of index.html start buton
-start.addEventListener("click", beginQuiz());
-
+// array for keep track of current question to display
+let currentQuestion = 0;
+let currentImage = 0;
 beginQuiz();
-console.log(questions);
+
+//---------------------------------------------------------------
+// empty array to store selection responses
+let responses = [];
+
+
+
+

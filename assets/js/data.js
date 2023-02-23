1ee00aa7883a9c59fdf25d5c4393fdbbc57a6dad
ThomasBallardCI <ThomasGEBallard@gmail.com>
2023-02-23 19:22:26 +0000

    JS added for quiz

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,2 +1,118 @@
+const answerBtnA = document.getElementById('a');
+const answerBtnB = document.getElementById('b');
+const answerBtnC = document.getElementById('c');
+const submitBtn = document.getElementById('submit-btn')
+const question = document.getElementById('question');
+const image = document.getElementsByClassName('picture-area');
+const start = document.getElementsByClassName('start-btn');
+const selected = document.getElementsByClassName('selected');
+
+// Results panel Hidden
+const resultsPanel = document.getElementById("panel-2");
+resultsPanel.style.display = "none";
+
+//---------------------------------------------------------
+// button selection color change
+let button = document.getElementsByClassName("answer-btn");
+let addSelectedClass = function(){
+    removeSelectClass();
+    this.classList.add('selected');
+};
+
+let removeSelectClass = function (){
+    for (let i =0; i < button.length; i++) {
+        button[i].classList.remove('selected');
+    }
+};
+for (let i =0; i < button.length; i++) {
+    button[i].addEventListener("click", addSelectedClass);
+};
+
+// ---------------------------------------------------------
+// array of questions
+let questions = [
+    {
+        question: "What would you say your foot width is?",
+        imgSrc: "url('/assets/images/footwidthpicture.jpg')",
+        answers: [
+            {id: "a", text: "Narrow"},
+            {id: "b", text: "Regular"},
+            {id: "c", text: "Wide"}
+        ]
+    },
+    {
+        question: "What is your preferred price point?",
+        imgSrc: "url('/assets/images/PriceRange.png')",
+        answers: [
+            {id: "a", text: "£100-£250"},
+            {id: "b", text: "£250-£400"},
+            {id: "c", text: "£400+"}
+        ]
+    },
+    {
+        question: "What style of skating do you want to do?",
+        answers: [
+            {id: "a", text: "Stunts"},
+            {id: "b", text: "Fitness"},
+            {id: "c", text: "Urban"}
+        ]
+    }
+];
+
+let currentQuestion = 0; // variable to keep track of current question
+
+// function to start quiz (to be called)
+function beginQuiz(){
+    loadQuestion(currentQuestion);
+}
+
+// function to load question data
+function loadQuestion(questionIndex) {
+    question.innerHTML = questions[questionIndex].question;
+    answerBtnA.innerHTML = questions[questionIndex].answers[0].text;
+    answerBtnB.innerHTML = questions[questionIndex].answers[1].text;
+    answerBtnC.innerHTML = questions[questionIndex].answers[2].text;
+    image[0].style.backgroundImage = questions[currentQuestion].imgSrc;
+    
+    // add event listener to submit button
+    submitBtn.addEventListener("click", function() {
+        // get selected answer
+        let selectedAnswer = document.querySelector(".selected");
+        // check if answer is selected
+        if (selectedAnswer) {
+            // log selected answer
+            console.log(`Question ${currentQuestion+1}: ${selectedAnswer.id}`);
+            // move to next question
+            currentQuestion++;
+            // clear selection
+            removeSelectClass();
+            // check if there are more questions
+            if (currentQuestion < questions.length) {
+                loadQuestion(currentQuestion);
+            } else {
+                    // hide quiz panel
+                    document.getElementById("panel-1").style.display = "none";
+                    // show results panel
+                    resultsPanel.style.display = "block";
+            }
+        }
+    });
+}
+
+// Restart quiz function
+function restartQuiz() {
+    // Reset current question
+    currentQuestion = 0;
+    // Reset answers array
+    answers = [];
+    // Remove displayed user's answers
+    document.getElementById("user-answers").innerHTML = "";
+    // Hide results panel
+    document.getElementById("panel-2").style.display = "none";
+    // Load first question
+    loadQuestion(currentQuestion);
+}
 
 
+// call beginQuiz to start the quiz
+beginQuiz();
--- assets/js/script.js
+++ assets/js/script.js
@@ -20,11 +20,12 @@ let addSelectedClass = function(){
 
 let removeSelectClass = function (){
     for (let i =0; i < button.length; i++) {
-        button[i].classList.remove('selected');
+        button[i].classList.remove('selected');    
     }
 };
 for (let i =0; i < button.length; i++) {
     button[i].addEventListener("click", addSelectedClass);
+    // return answers-btn.target.value;
 };
 
 // ---------------------------------------------------------
@@ -46,6 +47,7 @@ let questions = [
     }
 ];
 
+let answers = [];
 
 // function to start quizz (to be called)
 function beginQuiz(){
@@ -58,6 +60,34 @@ function loadQuestion(currentQuestion) {
     answerBtnA.innerHTML = questions[currentQuestion].answers[0];
     answerBtnB.innerHTML = questions[currentQuestion].answers[1];
     answerBtnC.innerHTML = questions[currentQuestion].answers[2];
+
+
+ // add event listener to submit button
+ submitBtn.addEventListener("click", function() {
+    // get selected answer
+    let selectedAnswer = document.querySelector(".selected").innerHTML;
+    // store selected answer in answers array
+    answers.push(selectedAnswer);
+    // clear selection
+    removeSelectClass();
+    // move to next question or display results
+    if (currentQuestion < questions.length - 1) {
+        loadQuestion(currentQuestion + 1);
+    } else {
+        displayResults();
+    }
+});
+}
+
+function displayResults() {
+// display results panel
+document.getElementById("panel-2").style.display = "block";
+// display user's answers
+for (let i = 0; i < answers.length; i++) {
+    let answer = document.createElement("p");
+    answer.innerHTML = answers[i];
+    document.getElementById("user-answers").appendChild(answer);
+}
 }
 
 // array for keep track of current question to display
@@ -65,9 +95,10 @@ let currentQuestion = 0;
 let currentImage = 0;
 beginQuiz();
 
+
 //---------------------------------------------------------------
-// empty array to store selection responses
-let responses = [];
+
+console.log(answers);
 
     // read selected class log the Value
     // interate to the next question

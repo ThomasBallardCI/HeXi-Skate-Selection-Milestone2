8a4844d3f8c468a5f26cdf5b64be7a597e3f8fd5
ThomasBallardCI <ThomasGEBallard@gmail.com>
2023-02-24 10:38:24 +0000

    re hid reults panel

--- assets/js/data.js
+++ assets/js/data.js
@@ -8,7 +8,7 @@ const start = document.getElementsByClassName('start-btn');
 const selected = document.getElementsByClassName('selected');
 
 // Results panel Hidden
-const resultsPanel = document.getElementById("panel-1");
+const resultsPanel = document.getElementById("panel-2");
 resultsPanel.style.display = "none";
 
 //---------------------------------------------------------
